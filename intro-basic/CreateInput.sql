e5f9d681914a9cc14535e618c93e20d8163cae6e
lgope <ilakshmangope@gmail.com>
2020-06-06 20:02:31 +0600

    create table columns 🛠

--- intro-basic/CreateInput.sql
+++ intro-basic/CreateInput.sql
@@ -31,3 +31,55 @@ CREATE TABLE table_name
 
     -- delete table
     DROP TABLE table_name;
+
+
+    -- 1) Create a table named 'results' with 3 VARCHAR columns called track, artist, and album, with lengths 200, 120, and 160, respectively.
+
+    -- Create the table
+    CREATE TABLE results
+    (
+        -- Create track column
+        track VARCHAR(200),
+        -- Create artist column
+        artist VARCHAR(120),
+        -- Create album column
+        album VARCHAR(160),
+    );
+
+    -- 2) Create one integer column called track_length_mins.
+    -- Create the table
+    CREATE TABLE results
+    (
+        -- Create track column
+        track VARCHAR(200),
+        -- Create artist column
+        artist VARCHAR(120),
+        -- Create album column
+        album VARCHAR(160),
+        -- Create track_length_mins
+        track_length_mins int,
+    );
+
+    -- 3) SELECT all the columns from your new table. No rows will be returned, but you can confirm that the table has been created.
+
+    -- Create the table
+    CREATE TABLE results
+    (
+        -- Create track column
+        track VARCHAR(200),
+        -- Create artist column
+        artist VARCHAR(120),
+        -- Create album column
+        album VARCHAR(160),
+        -- Create track_length_mins
+        track_length_mins INT,
+    );
+
+    -- Select all columns from the table
+    SELECT
+        track,
+        artist,
+        album,
+        track_length_mins
+    FROM
+        results;
