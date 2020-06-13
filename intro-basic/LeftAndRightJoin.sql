d21053c0a797189f83ce828a8caa96a761440290
lgope <ilakshmangope@gmail.com>
2020-06-13 16:56:49 +0600

    Fixed a bug of left join

--- intro-basic/LeftAndRightJoin.sql
+++ intro-basic/LeftAndRightJoin.sql
@@ -77,5 +77,5 @@ SELECT
 FROM album
     INNER JOIN artist ON album.artist_id = artist.artist_id
     -- Perform the correct join type to return matches or NULLS from the track table
-    RIGHT JOIN track on track.album_id = album.album_id
+    LEFT JOIN track on album.album_id = track.album_id
 WHERE album.album_id IN (213,214)
